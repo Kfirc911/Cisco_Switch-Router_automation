9d9cdebaf0714230514bcec52a496ff1b7bb6d2e
Kfir Cohen <KCOHEN07@Teva.Corp>
2022-05-29 16:13:59 +0300

    this is the main usage of show command for troubleshoot

---
+++ show_cpu.py
@@ -0,0 +1,16 @@
+from netmiko import ConnectHandler
+
+cisco_sw = {
+    'device_type': 'cisco_ios',
+    'host': input("please write the ip or the name of the switch : "),
+    """                                                                                               
+    in the user name and password you will need change and add you're admin permission.               
+    """
+    'username': '**********',
+    'password': '**********',
+}
+
+net_connect = ConnectHandler(**cisco_sw)
+
+output = net_connect.send_command("show processes cpu history")
+print(output)
